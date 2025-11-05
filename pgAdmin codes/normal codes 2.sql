dbd8a1b83e826f0a4270b636533d696723560ac7
Sathija <sathija2000@gmail.com>
2025-11-05 09:29:51 +0530

    Initial Commit

--- pgAdmin codes/normal codes 2.sql
+++ pgAdmin codes/normal codes 2.sql
@@ -8,3 +8,22 @@ select * from login_audit;
 
 select * from flyway_schema_history;
 
+
+
+
+select * from attendance_logs;
+
+select * from leave_requests;
+
+select * from leave_audit;
+
+select * from leave_balances;
+
+select * from leave_policies;
+
+
+
+DELETE FROM leave_balances
+WHERE id = 9;
+
+
